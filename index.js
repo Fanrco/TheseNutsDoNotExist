dd6cc163d669d7fdc47b1a0ea3e6d6ca5cdbe9de
Fanrco <fmira008@ucr.edu>
2022-10-11 01:36:39 -0700

    added nut info

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function readTextFromFile(path){
+    var request = new XMLHttpRequest();
+    request.open("GET", path, false);
+    request.send(null);
+    var returnValue = request.responseText;
+
+    return returnValue;
+}
+
 function getRandomImage(){
     console.log("got image");
 
+    //get random nut image
     let whichNut = getRandomInt(nNuts);
     var img = new Image();
     img.src = "nuts/Nuts"+whichNut+".png"
     document.getElementById("nutPic").appendChild(img);
+
+    let yamltext = readTextFromFile("nuts/Nuts"+whichNut+".yaml");
+    document.getElementById("yamltext").innerHTML = yamltext;
 
 }
